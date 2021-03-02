321515eca3fc56716ad5668897f00b4347c97b6e
Harper Reed <harper@nata2.org>
2021-03-02 02:42:33 +0000

    cleaned up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,6 @@
 <template>
   <div>
     <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
       <div class="container column is-10">
         <nuxt />
       </div>
@@ -23,17 +10,7 @@
 
 <script>
 export default {
-  data() {
-    return {
-      items: [
-        {
-          title: "Home",
-          icon: "home",
-          to: { name: "index" }
-        }
-      ]
-    };
-  }
+
 };
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <section class="section"></section>
+  <section class="section">
+    <h1 class="title">Hello!</h1>
+    <p class="block">My name is Harper Reed</p>
+    <p class="block">
+      This site is hosted on IPFS and powered by information stored on the ethereum blockchain
+      transactions.
+    </p>
+    <h1 class="title">About Harper Reed</h1>
+
+    <p class="block">
+      I am a computer operator who likes to make things. Also pranks.
+    </p>
+
+    <p class="block">
+      You can learn more about me at my website harperreed.com.
+    </p>
+
+    <p class="block">Follow me on:
+      <ul>
+        <li>Twitter</li>
+        <li>Instagram</li>
+        <li>My blog</li>
+      </ul>
+    </p>
+    <p class="block">
+      Feel free to reach out via Twitter or send me an email @
+      harper@modest.com.
+    </p>
+  </section>
 </template>
 
 <script>
@@ -7,7 +35,6 @@ import Card from "~/components/Card";
 
 export default {
   name: "HomePage",
-
   components: {
     Card
   }
