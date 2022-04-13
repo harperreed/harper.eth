7e7937ff1e19db25891d47cd2dfd3bb2800d0a1f
Harper Reed <harper@nata2.org>
2022-04-13 12:14:42 -0500

    tweaked NFT display

--- components/Nifty.vue
+++ components/Nifty.vue
@@ -14,21 +14,43 @@
           />
 
           <p class="font-semibold my-2">
-            <span class="font-medium">{{ asset.asset_contract.name }}:</span>
-            <span class="font-normal">{{ asset.name }}</span>
-          </p>
+            <span class="font-semibold">{{ asset.asset_contract.name }}:</span>
 
-          <p>
-            <!-- {{ asset.description }} -->
+            <span class="font-normal">{{ asset.name }}</span>
+
+          </p>
+          <p class="text-ellipsis overflow-hidden"> {{ asset.description }}</p>
+
+          <div class="flex flex-row justify-around mt-5">
+
             <!-- <pre>
             {{asset}}
             </pre> -->
 
+
             <!-- <br /> -->
-            <a :href="asset.permalink" target="_blank" class="text-blue-400"
-              >view on Opensea</a
+            <a :href="`https://search.fancy.art/token/${asset.asset_contract.address}/${asset.token_id}`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconFancy /></a
             >
-          </p>
+            <a :href="`https://opensea.io/assets/${asset.asset_contract.address}/${asset.token_id}`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconOpensea /></a
+            >
+            <a :href="`https://looksrare.org/collections/${asset.asset_contract.address}/${asset.token_id}`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconLooksRare /></a
+            >
+            <a :href="`https://rarible.com/token/${asset.asset_contract.address}:${asset.token_id}?tab=details`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconRarible /></a
+            >
+            <a :href="`https://pro.nansen.ai/nft-item-profiler?nft_address=${asset.asset_contract.address}&nft_id=${asset.token_id}`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconNansen /></a
+            >
+            <a :href="`https://etherscan.io/token/${asset.asset_contract.address}?a=${asset.token_id}`" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconEtherscan /></a
+            >
+            <a v-if="asset.external_link" :href="asset.external_link" target="_blank" class="text-gray-400 hover:text-blue-400"
+              ><IconLink /></a
+            >
+          </div>
         </div>
       </div>
     </div>
@@ -60,5 +82,3 @@ export default {
   methods: {},
 };
 </script>
-<style scoped>
-</style>
